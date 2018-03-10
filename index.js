5de51837226cd8cb1a04f18f8736036db6249076
vijay khatri <vijaykhatri22@gmail.com>
2018-03-10 08:50:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeliLine = [];
 function takeANumber (katzDeliLine,customer){
   katzDeliLine.push(customer);
-  return katzDeliLine.length;
+  return `Welcome, ${customer}. You are number ${katzDeliLine.length} in line.`;
 }
